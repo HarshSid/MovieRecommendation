1cd638d829f1e6b670f6d6aa439a2ec01b02e85f
Harsh <harshsiddhant70@gmail.com>
2020-05-31 19:56:00 +0530

    Add files via upload

---
+++ sf.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, redirect, url_for, request
+app = Flask(__name__)
+
+
+@app.route('/welcome_to')
+def welcome():
+    return render_template('welcome_to.html')
+
+
+
+
+if __name__ == '__main__':
+   app.debug = True
+   app.run()
+   app.run(debug = True)
